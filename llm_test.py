c73d7853934b701f0d9fab694456355de0805a09
Ansh Singh <anshsingh200516@Anshs-MacBook-Pro-7.local>
2025-06-02 17:42:44 +0200

    tst1

--- llm_test.py
+++ llm_test.py
@@ -11,7 +11,6 @@ load_dotenv
 OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 client = AsyncOpenAI(api_key=OPENAI_API_KEY)  
 
-test="hellooo"
 
 class LLM(SingleRequestModel, BatchRequestModel):
     def __init__(self,client:AsyncOpenAI,model:str='gpt-4'):
